bdb4d8cce022968ebfe1c7f3a15de30cc473f595
JGuzman40 <ticguzman78@gmail.com>
2024-10-21 03:12:41 -0500

    Users y Login listos con autenticación local

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const userRoutes = require("./userRoutes");
+const loginRoutes = require("./loginRoutes");
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/login", loginRoutes);
 
 module.exports = router;
